ac089e37be6af03b7fc7887bd7fa08299b29f99d
parthnabar <67985747+parthnabar@users.noreply.github.com>
2020-08-22 17:27:53 -0700

    updates to linear reg notebook

--- ds/linear_reg.py
+++ ds/linear_reg.py
@@ -1,6 +1,15 @@
+
 import pandas as pd
-
+# Reading and displaying data
 df = pd.read_csv (r'real_estate.csv')
-df.head()
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+print(df.head(5))
 
 
+
+
+x = df[1,2]
+print(x)
+#y = data[:,-1]
